0b1d9d4390341ef9f96657bc3e2faca12d9d42e5
Наиль Шаряпов <zolman99@gmail.com>
2020-01-25 15:14:34 +0300

    Updates

---
+++ precache-manifest.f49b8cab8475164904c0422317dd7302.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "586f6b0175995ba5644d260fbe37e543",
+    "url": "/react-repository/index.html"
+  },
+  {
+    "revision": "37d6520b060b8be8e7d3",
+    "url": "/react-repository/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "17ea943df2b7498308c5",
+    "url": "/react-repository/static/js/2.beffd31a.chunk.js"
+  },
+  {
+    "revision": "37d6520b060b8be8e7d3",
+    "url": "/react-repository/static/js/main.9785cd2a.chunk.js"
+  },
+  {
+    "revision": "3b18ec282c7bb871e3b3",
+    "url": "/react-repository/static/js/runtime-main.7e2607bd.js"
+  }
+]);
